76fe1ea6f00f0479a2a5fcbd12f1e377c02683a5
arusch <arusch@geninfo.com>
2017-05-23 22:43:49 -0400

    first commit

---
+++ StringListRemoveRedundancyExample/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringListRemoveRedundancyExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new List<string>() {"one", "two", "three", "two", "four", "three"};
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.WriteLine();
+
+            var uniqueWords1 = StringListRedundancyRemover.RemoveRedundancy1(words);
+
+            foreach (var uniqueWord in uniqueWords1)
+            {
+                Console.WriteLine(uniqueWord);
+            }
+
+            Console.WriteLine();
+
+            var uniqueWords2 = StringListRedundancyRemover.RemoveRedendancy2(words);
+
+            foreach (var uniqueWord in uniqueWords2)
+            {
+                Console.WriteLine(uniqueWord);
+            }
+
+            Console.WriteLine();
+
+            var uniqueWords3 = words.Distinct().ToList();
+
+            foreach (var uniqueWord in uniqueWords3)
+            {
+                Console.WriteLine(uniqueWord);
+            }
+        }
+    }
+}
---
+++ StringListRemoveRedundancyExample/StringListRedundancyRemover.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringListRemoveRedundancyExample
+{
+    class StringListRedundancyRemover
+    {
+        public static List<string> RemoveRedundancy1(List<string> words)
+        {
+            var uniqueWords = new List<string>();
+            var isUnique = true;
+
+            foreach(var word in words)
+            {
+                foreach(var uniqueWord in uniqueWords)
+                {
+                    if (word.Equals(uniqueWord))
+                    {
+                        isUnique = false;
+                        break;
+                    }
+                }
+
+                if (isUnique)
+                {
+                    uniqueWords.Add(word);
+                }
+                isUnique = true;
+            }
+            
+            return uniqueWords;
+        }
+
+        public static List<string> RemoveRedendancy2(List<string> words)
+        {
+            var uniqueWords = new List<string>();
+            var trackingWords = new List<string>();
+            trackingWords.AddRange(words);
+
+            words.ForEach(s =>
+            {
+                if (trackingWords.Contains(s))
+                {
+                    uniqueWords.Add(s);
+                    trackingWords.RemoveAll(t => t.Equals(s));
+                }
+            });
+
+            return uniqueWords;
+        }
+    }
+}
